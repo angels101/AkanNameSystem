5f6984e615e426d1f93069761cfb14dd6c24ec06
Angels101 <angelscodex101@gmail.com>
2020-07-17 20:31:50 +0300

    defining the if else if and else of the date

--- js/script.js
+++ js/script.js
@@ -7,5 +7,13 @@ var month = parseInt(document.getElementById("month").value) -1;
 var year = parseInt(document.getElementById("year").value);
 
 
-if (day < 0 )
+if (day < 0 ||day > 31 ) {
+    document.getElementsByClassName("dayerror").innerHtml ="Enter valid date";
+    return false;
+} else if (date == "") {
+    document.getElementsByClassName("dayerror").innerHtml = "This feild canot be empty";
+    return false;
+} else{
+    d.setDate(date);
 }
+
